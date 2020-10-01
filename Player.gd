6c0fec44a34a90978a7c8f3e9b640380c989b2d9
Jordan Halase <jordan@halase.me>
2020-10-01 16:33:10 -0500

    Adjust player movement and collision shape along with map

--- Player.gd
+++ Player.gd
@@ -1,36 +1,39 @@
 extends KinematicBody2D
 
-var velocity = Vector2()
 const UP = Vector2(0 , -1)
-const GRAVITY = 20
-const MAX_SPEED = 200
-const JUMP_HEIGHT = -500
-const ACCELERATION = 50
+const GRAVITY = 4
+const MAX_HSPEED = 80
+const MAX_VSPEED = 168
+const JUMP_HEIGHT = 164
+const ACCELERATION = 6
+const FRICTION_COEF = 0.16
+const FRICTION = ACCELERATION*FRICTION_COEF
+
+var velocity: Vector2 = Vector2()
+onready var map = get_parent()
 
 func _physics_process(_delta: float) -> void:
 	# Integrate gravity using forward Euler method
-	velocity.y += GRAVITY
-	var friction = true
+	velocity.y = min(MAX_VSPEED, velocity.y + GRAVITY)
 	
 	if Input.is_action_pressed("ui_right"):
 		# player moving right
-		velocity.x = min(velocity.x + ACCELERATION, MAX_SPEED)
+		velocity.x = min(velocity.x + ACCELERATION, MAX_HSPEED)
 		
 	elif Input.is_action_pressed("ui_left"):
 		# player moving left
-		velocity.x = max(velocity.x - ACCELERATION, -MAX_SPEED)
+		velocity.x = max(velocity.x - ACCELERATION, -MAX_HSPEED)
 	
 	# Check if the player is on the floor
 	if is_on_floor():
 		# velocity.y = 0
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up"):
 			# player jumping
-			velocity.y = JUMP_HEIGHT
-		if friction == true:
-			velocity.x = lerp(velocity.x, 0, 0.2)
-	else: 
-		if friction == true:
-			velocity.x = lerp(velocity.x, 0, 0.05)
+			velocity.y = -JUMP_HEIGHT
+		if velocity.x >= 0:
+			velocity.x = max(0, velocity.x - FRICTION)
+		else:
+			velocity.x = min(0, velocity.x + FRICTION)
 			
 	velocity = move_and_slide(velocity, UP)
-	get_parent().level_wrap(self)
+	map.level_wrap(self)
