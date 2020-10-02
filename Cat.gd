e6db096056c9da7f45cb3c69b21f6d1496ff6b8f
Jordan Halase <jordan@halase.me>
2020-10-01 21:33:48 -0500

    Do not collide enemy and player

--- Cat.gd
+++ Cat.gd
@@ -15,7 +15,7 @@ var direction = 1
 
 var is_in_animation = false
 
-onready var player = get_node("../Player") 
+onready var player = get_node("../Player")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
