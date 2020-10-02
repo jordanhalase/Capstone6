d377b829538705e088b07e15a6d75a3813d80a48
Jordan Halase <jordan@halase.me>
2020-10-02 13:47:39 -0500

    Refactor and incorporate

--- BirdStationary.gd
+++ BirdStationary.gd
@@ -1,7 +1,6 @@
 extends "res://HUD.gd"
 
-func _on_TestingPoint_body_entered(body):
+func _on_BirdStationary_body_entered(body):
 	if body.name == "Player":
 		get_node("/root/Hud").score += 100
 		queue_free()
-	pass # Replace with function body.
