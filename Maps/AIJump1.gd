00f24eb9f5da8139fd0c27ae397edadeac05f1af
Jordan Halase <jordan@halase.me>
2020-10-02 12:31:35 -0500

    Tweak jumpAcrossVel

--- Maps/AIJump1.gd
+++ Maps/AIJump1.gd
@@ -7,5 +7,5 @@ export var jumpAcross: bool = true
 export var jumpBelow: bool = true
 
 export var jumpAboveVel: float = 152
-export var jumpAcrossVel: float = 100
+export var jumpAcrossVel: float = 80
 export var jumpBelowVel: float = 20
