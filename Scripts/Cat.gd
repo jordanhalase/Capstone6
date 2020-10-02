b4e1029c255f8e2dbfb6b2be803924b450379292
Jordan Halase <jordan@halase.me>
2020-10-02 16:38:25 -0500

    Draw cats in correct direction

--- Scripts/Cat.gd
+++ Scripts/Cat.gd
@@ -5,7 +5,7 @@ const SPEED: float = 72.0
 const UP := Vector2(0, -1)
 
 # Maximum distance from the player that the cat will turn around
-const MAX_DISTANCE: float = 120.0
+const MAX_DISTANCE: float = 240.0
 
 # Give a small range where the player can be considered on the same level
 # as the cat is
@@ -14,7 +14,7 @@ const EPSILON: float = 8.0
 var velocity := Vector2()
 
 # This is used to index an array where 0 is left and 1 is right
-var direction: bool = true
+export var facesRight: bool = true
 
 # Used to prevent multiple collisions with the same AI block
 # before completing the decided move
@@ -32,6 +32,7 @@ onready var map := get_parent()
 var callback_params = [0.0, false]
 
 func _ready() -> void:
+	$Sprite.set_flip_h(!facesRight)
 	walk()
 
 func _physics_process(_delta: float) -> void:
@@ -39,7 +40,7 @@ func _physics_process(_delta: float) -> void:
 	velocity.y += GRAVITY
 	
 	if is_on_wall():
-		direction = !direction
+		flip_direction()
 	
 	if !thinking:
 		walk()
@@ -58,19 +59,22 @@ func sit():
 	
 func jump(jumpY):
 	velocity.y = -jumpY
-	print("Cat jumped ", jumpY, " units")
 	thinking = false
 	
 func walk():
-	if direction:
+	if facesRight:
 		velocity.x = SPEED
 	else:
 		velocity.x = -SPEED
 
+func flip_direction():
+	facesRight = !facesRight
+	$Sprite.set_flip_h(!facesRight)
+
 # Decision callback from AIBlock
 func _decide(ai):
 	if !thinking:
-		if direction == ai.facesRight:
+		if facesRight == ai.facesRight:
 			thinking = true
 			if position.y > (player.position.y + EPSILON) && ai.jumpAbove:
 				callback_params = [ai.jumpAboveVel, false]
@@ -78,7 +82,7 @@ func _decide(ai):
 				callback_params = [ai.jumpBelowVel, false]
 			else:
 				var dx = position.x - player.position.x
-				if (dx > 0 && dx < MAX_DISTANCE && direction == true) || (dx < 0 && dx > -MAX_DISTANCE && direction == false):
+				if (dx > 0 && dx < MAX_DISTANCE && facesRight == true) || (dx < 0 && dx > -MAX_DISTANCE && facesRight == false):
 					callback_params = [null, true]
 				elif ai.jumpAcross:
 					callback_params = [ai.jumpAcrossVel, false]
@@ -87,7 +91,6 @@ func _decide(ai):
 					walk()
 			if thinking:
 				$Timer.start()
-				print("sit")
 				sit()
 
 # Timer callback
@@ -95,7 +98,7 @@ func _on_Timer_timeout():
 	var jumpY = callback_params[0]
 	var changed_direction = callback_params[1]
 	if changed_direction:
-		direction = !direction
+		flip_direction()
 		reversed = true
 	else:
 		jump(jumpY)
