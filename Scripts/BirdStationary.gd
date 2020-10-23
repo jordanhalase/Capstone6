1d89da6521ca314a3bde9bd057e74b7e34e3adf0
Jordan Halase <jordan@halase.me>
2020-10-23 13:37:52 -0500

    Emit bird_collected signal

--- Scripts/BirdStationary.gd
+++ Scripts/BirdStationary.gd
@@ -1,7 +1,10 @@
 extends "res://Scripts/HUD.gd"
 
+signal bird_collected
+
 func _on_BirdStationary_body_entered(body):
 	if body.name == "Player":
 		get_node("../BirdChain").increment_chain()
 		get_node("/root/Hud").score += 100
+		emit_signal("bird_collected")
 		queue_free()
