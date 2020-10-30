2500d0403e74ef14ae9db32be88c234fa19923a7
Jordan Halase <jordan@halase.me>
2020-10-30 14:09:56 -0500

    DyingCat doesn't move because it's a child of Weapon_In_Motion

--- Scripts/DyingCat.gd
+++ Scripts/DyingCat.gd
@@ -1,18 +1,7 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-var velocity = Vector2(0,0)
 var facesRight = true
-#TODO: Add the horizantal movement then disappear!
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position.y -= 8
 	if facesRight: $AnimatedSprite.flip_h = true
-#	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	translate(velocity)
